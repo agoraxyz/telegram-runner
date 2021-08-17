ad105fd02c4621ccef7792ce900596d26075486a
dezsim <dezsimartin@gmail.com>
2021-08-18 00:54:33 +0200

    removed unused scanMatchAsync

--- src/database/RedisClient.ts
+++ src/database/RedisClient.ts
@@ -14,17 +14,6 @@ export default class RedisClient {
       });
     });
 
-  scanMatchAsync = async (pattern: string): Promise<string[]> =>
-    new Promise((resolve, reject) => {
-      this.client.scan("0", "match", pattern, (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result[1]);
-        }
-      });
-    });
-
   constructor(client: redis.RedisClient) {
     this.client = client;
   }
