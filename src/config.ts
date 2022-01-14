dbb336f727e8ab4ac2333509706f4c29ac03151a
dezsim <dezsimartin@gmail.com>
2022-01-14 15:50:14 +0100

    removed unused env-s from config

--- src/config.ts
+++ src/config.ts
@@ -9,9 +9,6 @@ if (envFound.error && !process.env.BOT_TOKEN)
   throw new Error("Couldn't find .env file or volumes in compose.");
 */
 
-const redisHost = process.env.REDIS_HOST;
-const hmacAlgorithm = process.env.HMAC_ALGORITHM || "sha256";
-const hmacSecret = process.env.HMAC_SECRET;
 const telegramToken = process.env.BOT_TOKEN;
 const backendUrl = process.env.BACKEND_URL;
 const mtprotoApiId = process.env.MTPROTO_API_ID;
@@ -30,12 +27,6 @@ if (!telegramToken)
 if (!backendUrl)
   throw new Error("You need to specify the BACKEND_URL in the .env file.");
 
-if (!redisHost)
-  throw new Error("You need to specify the REDIS_HOST in the .env file.");
-
-if (!hmacSecret)
-  throw new Error("You need to specify the HMAC_SECRET in the .env file.");
-
 if (!mtprotoApiId)
   throw new Error("You need to specify the MTPROTO_API_ID in the .env file.");
 
@@ -43,9 +34,6 @@ if (!mtprotoApiHash)
   throw new Error("You need to specify the MTPROTO_API_HASH in the .env file.");
 
 export default {
-  redisHost,
-  hmacAlgorithm,
-  hmacSecret,
   telegramToken,
   backendUrl,
   api,
