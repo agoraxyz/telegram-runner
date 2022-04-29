916743625de3574b56ee393d8c03c64b26700737
richardanyalai <gtaricsi@gmail.com>
2022-04-29 11:11:10 +0200

    Updated package-lock.json and config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,10 +4,10 @@
 import * as dotenv from "dotenv";
 
 const envFound = dotenv.config();
-/*
-if (envFound.error && !process.env.BOT_TOKEN)
+
+if (!envFound) {
   throw new Error("Couldn't find .env file or volumes in compose.");
-*/
+}
 
 const telegramToken = process.env.BOT_TOKEN;
 const botUsername = process.env.BOT_USERNAME;
@@ -20,14 +20,17 @@ const supergroupVideo = process.env.SUPERGROUP_VIDEO_URL;
 const adminVideo = process.env.ADMIN_VIDEO_URL;
 const groupIdImage = process.env.GROUPID_IMAGE;
 
-if (!telegramToken)
+if (!telegramToken) {
   throw new Error("You need to specify the bot's BOT_TOKEN in the .env file.");
+}
 
-if (!botUsername)
+if (!botUsername) {
   throw new Error("You need to specify the BOT_USERNAME in the .env file.");
+}
 
-if (!backendUrl)
+if (!backendUrl) {
   throw new Error("You need to specify the BACKEND_URL in the .env file.");
+}
 
 export default {
   telegramToken,
