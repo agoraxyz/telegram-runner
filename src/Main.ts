4a4656d4d0b37a01d9a216632d39bd03876501d5
richardanyalai <gtaricsi@gmail.com>
2022-06-29 11:28:17 +0200

    Added guild sdk

--- src/Main.ts
+++ src/Main.ts
@@ -1,16 +1,25 @@
 import axios from "axios";
+import { Platform, setApiBaseUrl } from "@guildxyz/sdk";
 import api from "./api/api";
 import Bot from "./Bot";
 import config from "./config";
+import logger from "./utils/logger";
 import { logAxiosResponse } from "./utils/utils";
 
 export default class Main {
-  static start(): void {
-    // start listener
-    api();
+  public static platform: Platform;
 
+  public static async start(): Promise<void> {
     // log all axios responses
     axios.interceptors.response.use(logAxiosResponse);
+
+    // setup sdk
+    setApiBaseUrl(config.backendUrl);
+    logger.info(`Backend url set to ${config.backendUrl}`);
+    this.platform = new Platform(config.platform);
+
+    // start listener
+    api();
 
     // setup the Telegram bot
     Bot.setup(config.telegramToken);
